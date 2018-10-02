ee0e25e4c135e430c762818bd1663d661d921372
Karpov Nikolay <karpnv@gmail.com>
2018-10-02 18:08:44 +0300

    Add batch_size parameter to embedding

--- keras_text/models/token_model.py
+++ keras_text/models/token_model.py
@@ -70,7 +70,7 @@ class TokenModelFactory(object):
                                         mask_zero=True,
                                         trainable=trainable_embeddings)
         else:
-            embedding_layer = Embedding(len(self.token_index) + 1,
+            embedding_layer = Embedding(embedding_size,
                                         self.embedding_dims,
                                         weights=[build_embedding_weights(self.token_index, self.embeddings_index)],
                                         input_length=self.max_tokens,
